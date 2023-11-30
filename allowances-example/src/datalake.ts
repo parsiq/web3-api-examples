6c2e21de6ca9950ce7878905a60dcc28c94372cc
Ivan Ivanitskiy <ivanitskiy.ivan@gmail.com>
2023-11-30 20:02:35 +0400

    msg sender retrieval added, correct allowances and balances calculation

--- allowances-example/src/datalake.ts
+++ allowances-example/src/datalake.ts
@@ -3,60 +3,79 @@ import { TsunamiApiClient, ChainId } from '@parsiq/tsunami-client';
 import { TsunamiFilter, TsunamiEvent, DatalakeTsunamiApiClient } from '@parsiq/tsunami-client-sdk-http';
 import { Interface } from '@ethersproject/abi';
 import * as util from 'util';
+// import readline from 'readline';
 
 // Import your ABIs here, in a format suitable for decoding using @ethersproject/abi.
-import votingAbi from './ERC20.json';
+import ERC20Abi from './WETHAbi.json';
 
 // Put your Tsunami API key here.
-const TSUNAMI_API_KEY = 'YOUR_API_KEY';
+const TSUNAMI_API_KEY = 'mBjULRIIP4Qff5PpzoAEAVUJg1qE79SC';
 // This is the chain ID for Ethereum mainnet Tsunami API. Change it if you want to work with a different net.
-const TSUNAMI_API_NET = ChainId.ETH_MAINNET;
+const TSUNAMI_API_NET = ChainId.ETH_SEPOLIA;
 
 // The address of the wallet that you want to monitor for transfers and approvals.
-const WALLET_ADDRESS = '0x7db90684e72a9db431bd4a2a13fcc3412de4de96';
+const WALLET_ADDRESS = '0xE67ddd0Ef25BC9d6A2A55b4b5946140B9e570121';
 // USDT contract address for starters.
-const CONTRACT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const CONTRACT_ADDRESS = '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14';
+const CONTRACT_ADDRESS_LOWERCASE = '0xfff9976782d46cc05630d1f6ebab18b2324d6b14';
 
 // topic_0 hashes of our events of interest.
 const TRANSFER_EVENT_TOPIC_0 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
 const APPROVAL_EVENT_TOPIC_0 = '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925';
+const WETH_DEPOSIT_EVENT_TOPIC_0 = '0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c';
+
+// sig hashes of calls of interest.
+const TRANSFER_FROM_SIG_HASH = '0x23b872dd';
 
 // The block from which you want to monitor the wallet.
-const MONITORING_START_BLOCK_NUMBER = 18_419_531;
+const MONITORING_START_BLOCK_NUMBER = 4_567_462;
 
 // This defines the layout of the type-safe K-V storage.
 type DatalakeStorageLayout = {
     '': any,
-    balance: { _address: string, _balance: number },
-    allowance: { _owner_spender: string, _allowance: number },
+    balances: { address: string, balance: number },
+    allowances: { owner_spender: string, allowance: number },
+    transfers: { from: string, to: string, value: number },
+    transferFroms: { owner: string, spender: string, value: number },
+    approvals: { owner: string, spender: string, value: number },
+    deposits: { to: string, value: number },
 };
 
 type DatalakeStorageMetaLayout = {
     '': {},
-    balance: {},
-    allowance: {},
+    balances: {},
+    allowances: {},
+    transfers: {},
+    transferFroms: {},
+    approvals: {},
+    deposits: {},
 };
 
 // Types for decoded events follow.
 type TransferEvent = {
-    from: string,
-    to: string,
-    value: number,
+    src: string,
+    dst: string,
+    wad: number,
 }
 
 type ApprovalEvent = {
-    owner: string,
-    spender: string,
-    value: number,
+    src: string,
+    guy: string,
+    wad: number,
+}
+
+type DepositEvent = {
+    dst: string,
+    wad: number,
 }
 
 class Datalake extends sdk.AbstractMultiStorageDataLakeBase<DatalakeStorageLayout, DatalakeStorageMetaLayout, TsunamiFilter, TsunamiEvent> {
-    private votingDecoder: Interface;
+    private ERC20Decoder: Interface;
 
     // Construct ABI decoders here.
     constructor() {
         super();
-        this.votingDecoder = new Interface(votingAbi);
+        this.ERC20Decoder = new Interface(ERC20Abi);
     }
 
     public override getProperties(): sdk.DataLakeProperties {
@@ -70,7 +89,7 @@ class Datalake extends sdk.AbstractMultiStorageDataLakeBase<DatalakeStorageLayou
     public async genTsunamiFilterForBlock(block: sdk.Block & sdk.DataLakeRunnerState, isNewBlock: boolean): Promise<TsunamiFilter> {
         return {
             contract: [CONTRACT_ADDRESS],
-            topic_0: [TRANSFER_EVENT_TOPIC_0, APPROVAL_EVENT_TOPIC_0],
+            topic_0: [TRANSFER_EVENT_TOPIC_0, APPROVAL_EVENT_TOPIC_0, WETH_DEPOSIT_EVENT_TOPIC_0],
         };
     }
 
@@ -83,37 +102,70 @@ class Datalake extends sdk.AbstractMultiStorageDataLakeBase<DatalakeStorageLayou
             case APPROVAL_EVENT_TOPIC_0:
                 await this.processApprovalEvent(event);
                 break;
+            case WETH_DEPOSIT_EVENT_TOPIC_0:
+                await this.processDepositEvent(event);
+                break;
         }
     }
 
     private async processTransferEvent(event: TsunamiEvent): Promise<void> {
         // Decodes the event.
-        const fragment = this.votingDecoder.getEvent(event.topic_0!);
-        const decoded = this.votingDecoder.decodeEventLog(fragment, event.log_data!, [
+        const fragment = this.ERC20Decoder.getEvent(event.topic_0!);
+        const decoded = this.ERC20Decoder.decodeEventLog(fragment, event.log_data!, [
             event.topic_0!,
             event.topic_1!,
             event.topic_2!
         ]) as unknown as TransferEvent;
+
         // Checks whether the event is relevant to the wallet we are monitoring.
         this.log('INFO', util.inspect(decoded));
-        if (decoded.from.toString() === WALLET_ADDRESS || decoded.to.toString() === WALLET_ADDRESS) {
+        const from = decoded.src.toString();
+        const to = decoded.dst.toString();
+        if (from === WALLET_ADDRESS || to === WALLET_ADDRESS) {
+            // Stores the event in Transfers table.
+            const value = decoded.wad;
+            await this.set('transfers', `${event.tx_hash}`, {
+                from: from,
+                to: to,
+                value: value,
+            });
             // Sets the sign of the balance change depending on whether the wallet is the sender or the receiver.
-            const sign = decoded.from.toString() === WALLET_ADDRESS ? -1 : 1;
+            const sign = from === WALLET_ADDRESS ? -1 : 1;
             // Updates the balance if it exists, or creates a new balance record.
-            const balance = await this.get('balance', decoded.to.toString());
+
+            const balance = await this.get('balances', WALLET_ADDRESS.toLowerCase());
             if (balance) {
-                balance._balance += sign * decoded.value;
-                await this.set('balance', decoded.to.toString(), balance);
+                balance.balance = Number(balance.balance) + sign * value;
+                await this.set('balances', WALLET_ADDRESS.toLowerCase(), balance);
             }
             else {
-                await this.set('balance', decoded.to.toString(), { _address: decoded.to.toString(), _balance: decoded.value });
+                await this.set('balances', WALLET_ADDRESS.toLowerCase(), { address: WALLET_ADDRESS.toLowerCase(), balance: sign * value });
             }
             // Updates the allowance if it exists and the wallet is the from address.
-            const allowance = await this.get('allowance', `${decoded.from.toString()}_${decoded.to.toString()}`);
-            if (allowance && decoded.from.toString() === WALLET_ADDRESS) {
-                if (allowance._allowance != 2**256 - 1) {
-                    allowance._allowance -= decoded.value;
-                    await this.set('allowance', `${decoded.from.toString()}_${decoded.to.toString()}`, allowance);
+            // Also checking that spender address is the caller address.
+            if (from === WALLET_ADDRESS) {
+                const tsunami = new TsunamiApiClient(TSUNAMI_API_KEY, TSUNAMI_API_NET);
+                const hash = event.tx_hash;
+                const tx = await tsunami.getTransactionWithLogs(hash);
+                let msg_sender;
+                // Get sender address from the transaction.
+                for (const call of tx.logs ?? []) {
+                    console.log('Current call: ', call);
+                    const right_call = call.op_code === 'CALL' && call.contract === CONTRACT_ADDRESS_LOWERCASE && call.sig_hash === TRANSFER_FROM_SIG_HASH;
+                    if (right_call) {
+                        msg_sender = call.sender;
+                        break;
+                    }
+                }
+                if (msg_sender) {
+                    const owner_spender = `${from}_${msg_sender}`.toLowerCase();
+                    const allowance = await this.get('allowances', owner_spender);
+                    if (allowance) {
+                        if (allowance.allowance != 2**256 - 1) {
+                            allowance.allowance = Number(allowance.allowance) - value;
+                            await this.set('allowances', owner_spender, allowance);
+                        }
+                    }
                 }
             }
         }
@@ -121,31 +173,84 @@ class Datalake extends sdk.AbstractMultiStorageDataLakeBase<DatalakeStorageLayou
 
     private async processApprovalEvent(event: TsunamiEvent): Promise<void> {
         // Decodes the event...
-        const fragment = this.votingDecoder.getEvent(event.topic_0!);
-        const decoded = this.votingDecoder.decodeEventLog(fragment, event.log_data!, [
+        const fragment = this.ERC20Decoder.getEvent(event.topic_0!);
+        const decoded = this.ERC20Decoder.decodeEventLog(fragment, event.log_data!, [
             event.topic_0!,
             event.topic_1!,
             event.topic_2!
         ]) as unknown as ApprovalEvent;
         // Checks whether the event is relevant to the wallet we are monitoring.
-        if (decoded.owner.toString() === WALLET_ADDRESS) {
+        const owner = decoded.src.toString();
+        if (owner === WALLET_ADDRESS) {
             // Updates the allowance if it exists, or creates a new allowance record.
-            const allowance = await this.get('allowance', `${decoded.owner.toString()}_${decoded.spender.toString()}`);
+            const spender = decoded.guy.toString();
+            const value = decoded.wad;
+            // Stores the event in Approvals table.
+            await this.set('approvals', `${event.tx_hash}`, {
+                owner: owner,
+                spender: spender,
+                value: value,
+            });
+            const owner_spender = `${owner}_${spender}`.toLowerCase();
+            const allowance = await this.get('allowances', owner_spender);
             if (allowance) {
-                allowance._allowance = decoded.value;
-                await this.set('allowance', `${decoded.owner.toString()}_${decoded.spender.toString()}`, allowance);
+                allowance.allowance = value;
+                await this.set('allowances', owner_spender, allowance);
             }
             else {
-                await this.set('allowance', `${decoded.owner.toString()}_${decoded.spender.toString()}`, { _owner_spender: `${decoded.owner.toString()}_${decoded.spender.toString()}`, _allowance: decoded.value });
+                await this.set('allowances', owner_spender, { owner_spender: owner_spender, allowance: value });
             }
         }
     }
+
+    private async processDepositEvent(event: TsunamiEvent): Promise<void> {
+        // Decodes the event...
+        const fragment = this.ERC20Decoder.getEvent(event.topic_0!);
+        const decoded = this.ERC20Decoder.decodeEventLog(fragment, event.log_data!, [
+            event.topic_0!,
+            event.topic_1!
+        ]) as unknown as DepositEvent;
+        // Checks whether the event is relevant to the wallet we are monitoring.
+        const to = decoded.dst.toString();
+        if (to === WALLET_ADDRESS) {
+            const value = decoded.wad;
+            // Stores the event in Deposits table.
+            await this.set('deposits', `${event.tx_hash}`, {
+                to: to,
+                value: value,
+            });
+            // Updates the balance if it exists, or creates a new balance record.
+            const balance = await this.get('balances', WALLET_ADDRESS.toLowerCase());
+            if (balance) {
+                balance.balance += value;
+                await this.set('balances', WALLET_ADDRESS.toLowerCase(), balance);
+            }
+            else {
+                await this.set('balances', WALLET_ADDRESS.toLowerCase(), { address: WALLET_ADDRESS.toLowerCase(), balance: value });
+            }
+        }
+    }
+    
 
     // The following event handlers should be no-ops under most circumstances.
     public async processEndOfBlockEvent(event: sdk.Block & sdk.DataLakeRunnerState): Promise<void> {}
     public async processBeforeDropBlockEvent(event: sdk.DropBlockData & sdk.DataLakeRunnerState): Promise<void> {}
     public async processAfterDropBlockEvent(event: sdk.DropBlockData & sdk.DataLakeRunnerState): Promise<void> {}
 }
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// const waitForUserInput = async () => {
+//     return new Promise<void>((resolve) => {
+//         rl.question('Press ENTER to continue with balance update...', () => {
+//             rl.close();
+//             resolve();
+//         });
+//     });
+// };
 
 export const run = async (): Promise<void> => {
     const logger = new sdk.ConsoleLogger();
@@ -159,8 +264,12 @@ export const run = async (): Promise<void> => {
     const runner = new sdk.MultiStorageDataLakeRunner({
         storageConfig: {
             '': { meta: {} },
-            'allowance': { meta: {} },
-            'balance': { meta: {} },
+            'allowances': { meta: {} },
+            'balances': { meta: {} },
+            'transfers': { meta: {} },
+            'transferFroms': { meta: {} },
+            'approvals': { meta: {} },
+            'deposits': { meta: {} },
         },
         datalake: datalake,
         tsunami: tsunamiSdk,
